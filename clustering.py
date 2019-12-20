190016e53f035c75ee917f64107daef396d01f62
santosh0420 <ee1160420.iitd@gmail.com>
2019-12-21 01:57:21 +0530

    commit

--- clustering.py
+++ clustering.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from datetime import datetime
 import math
+from matplotlib import pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
 import collections
@@ -10,56 +11,49 @@ import threading
 from multiprocessing.dummy import Pool as ThreadPool
 import ray
 import psutil
-import time
+import time	
+from sklearn.cluster import AgglomerativeClustering
+import numpy as np
+import hdbscan
 
-def removeDuplicates(lon, lat):
-	# x = [i for i in range(len(lat))]
-	# fig = px.scatter(x=lon,y=lat)
-	# fig.show()
-	#Starts here
-	dc = 50
-	visited = [False for i in range(len(lon))]
-	window = changeinlat(dc)
-	count=0
-	lon1 = []
+
+def main():
+	df = pd.read_csv('/home/s/Taxi/stops/stops.csv')
+	df = df.sort_values('Latitude')
+	lon0 = df['Longitude']
+	lat0 = df['Latitude']
+	n = 10000
+	lon = np.asarray(lon0[:n]).reshape(-1,1)
+	lat = np.asarray(lat0[:n]).reshape(-1,1)
+	points = np.concatenate((lat, lon), axis=1)*(6378137/180)*math.pi
+	clustering = AgglomerativeClustering(n_clusters=None, distance_threshold=40).fit(points)
 	lat1 = []
-	for i in range(len(lon)):
-		if(visited[i]==True):
-				continue
-		cluster_lat = [lat[i]]
-		cluster_lon = [lon[i]]
-		mean_lat = lat[i]
-		mean_lon = lon[i]
-		visited[i] = True
-		for j in range(len(lon)):
-			if(i==j):
-				continue
-			if(visited[j]==True):
-				continue
-			#checking if point j in in the dc distance window
-			if(mean_lon-window<lon[j]<mean_lon+window and mean_lat-window<lat[j]<mean_lat+window):
-				visited[j] = True
-				cluster_lon.append(lon[j])
-				cluster_lat.append(lat[j])
-				mean_lon = statistics.mean(cluster_lon)
-				mean_lat = statistics.mean(cluster_lat)
-		# if(len(cluster_lat)>1):
-			# print(statistics.stdev(cluster_lat))
-		if(len(cluster_lat)<30):
-			continue
-		lon1.append(mean_lon)
-		lat1.append(mean_lat)
-		count+=1
-	# x = [i for i in range(len(lon1))]
-	# fig = px.scatter(x=lon1,y=lat1)
+	lon1 = []
+	# plt.scatter(points[:, 0], points[:, 1], c=clustering.labels_,
+	#                         cmap=plt.cm.nipy_spectral)
+	# plt.show()
+	res = collections.Counter(clustering.labels_)
+	# print(res)
+	# print(clustering.labels_)
+	# print(res)
+	col = []
+	for i in range(n):
+		if(res[clustering.labels_[i]]>6):
+			lat1.append(points[i][0]*(180/(6378137*math.pi)))
+			lon1.append(points[i][1]*(180/(6378137*math.pi)))
+			col.append(clustering.labels_[i])
+	# print(len(lat1))
+	print(lon1)
+	# # lon1, lat1 = removeDuplicates(lon1, lat1)
+
+	# fig = px.scatter(x=points[:, 0], y=points[:, 1], color =clustering.labels_)
+	# fig = go.Figure(data=go.Scatter(
+ #    x=lat1,
+ #    y=lon1,
+ #    mode='markers',
+ #    marker=dict(color=col)))
+
 	# fig.show()
-	return lon1, lat1
 
-
-df = pd.read_csv('/home/s/Documents/Taxi/stops/stops.csv')
-
-lon = df['Longitude']
-lat = df['Latitude']
-lon1, lat1 = removeDuplicates(lon, lat)
-fig = px.scatter(x=lon1,y=lat1)
-fig.show()
+if __name__ == '__main__':
+	main()
