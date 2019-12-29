4a7c615a26b00479d36488db0b75fbb6e500b05f
santosh0420 <ee1160420.iitd@gmail.com>
2019-12-30 00:25:28 +0530

    update

---
+++ rm.sh
@@ -0,0 +1,28 @@
+sudo rm -r 1Jan
+mkdir 1Jan
+sudo rm -r 2Jan
+mkdir 2Jan
+sudo rm -r 3Jan
+mkdir 3Jan
+sudo rm -r 4Jan
+mkdir 4Jan
+sudo rm -r 5Jan
+mkdir 5Jan
+sudo rm -r 6Jan
+mkdir 6Jan
+sudo rm -r 7Jan
+mkdir 7Jan
+sudo rm -r 8Jan
+mkdir 8Jan
+sudo rm -r 26Dec
+mkdir 26Dec
+sudo rm -r 27Dec
+mkdir 27Dec
+sudo rm -r 28Dec
+mkdir 28Dec
+sudo rm -r 29Dec
+mkdir 29Dec
+sudo rm -r 30Dec
+mkdir 30Dec
+sudo rm -r 31Dec
+mkdir 31Dec
