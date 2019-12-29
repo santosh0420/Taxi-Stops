37a9bbea2ce9b00d7544009aa43290b699b1bb90
santosh0420 <ee1160420.iitd@gmail.com>
2019-12-29 23:05:26 +0530

    update

--- split.py
+++ split.py
@@ -61,7 +61,7 @@ def main():
 	for p in paths:
 		p1 = os.listdir(path+'/'+p)
 		for p2 in p1:
-			all_files.append(os.real_path(p2))
+			all_files.append(os.path.real_path(p2))
 	ray.get([split_in_files.remote(f) for f in all_files])
 	print('Complete')
 	end = time.time()
